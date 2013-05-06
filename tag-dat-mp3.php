c4f1d89c75be1abbb27d1dd79c2f4c4943e6f80b
Sony? <sony@4morefun.net>
2013-05-06 20:25:01 +0200

    Done.

--- tag-dat-mp3.php
+++ tag-dat-mp3.php
@@ -1,15 +1,69 @@
 <?php
 
-$folder = (isset($argv[1])) ? $argv[1] : '.';
+define('DS', '/');
 
 
+$folder   = (isset($argv[1])) ? $argv[1] : '.';
+$numFiles = 0;
+$sucesses = 0;
+$fails    = 0;
+$warnings = 0;
+
+
+//Boring settings and dependices
+require_once('vendor/getid3/getid3.php');
+require_once('vendor/getid3/write.php');
+
+$getID3 = new getID3;
+$getID3->setOption(array('encoding'=>'UTF-8'));
+
+$tagwriter                    = new getid3_writetags;
+$tagwriter->tagformats        = array('id3v2.3'); //array('id3v1', 'id3v2.3');
+$tagwriter->remove_other_tags = false;
+$tagwriter->tag_encoding      = 'UTF-8';
+
+
+
+//The real code
 foreach(glob($folder .'/*') as $artist)
 {
     foreach(glob($artist .'/*') as $song)
     {
+        $tagwriter->filename          = $song;
+        $tagwriter->tag_data          = array(
+            'title'         => array(pathinfo($song, PATHINFO_FILENAME)),
+            'artist'        => array(basename($artist))
+        );
 
+        echo "Processing  '". $song ."'".PHP_EOL;
+        
+
+        //Yay
+        if($tagwriter->WriteTags())
+            if($tagwriter->warnings)
+                $warnings++;
+            else
+                $sucesses++;
+        else
+            $fails++;
+
+
+        $numFiles++;
     }
 }
+
+
+
+echo <<<OUT
+Done.
+
+Successes: $sucesses
+Warnings:  $warnings
+Fails:     $fails
+
+Number of files: $numFiles
+
+OUT;
 
 
 ?>
